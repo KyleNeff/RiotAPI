0ceb1de80d4fa2dd48a5438fa951621566509df0
Kyle <kyleneff003@LAPTOP-3F0G1F93.localdomain>
2022-11-28 12:31:03 -0700

    Now shows solo q rank with level

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,42 @@
-import React from "react";
+//cd app
+//nvm install 18.12.1
+//npm start
+
+import React, { useState } from "react";
 import "./App.css";
 import SearchBar from "./SearchBar"
 import helmetIcon from './image1.jpg';
 import firePoroIcon from './image2.jfif';
 import LCSIcon from './image3.jpg';
-
+import axios from 'axios';
 
 function App(){
+  const apiKey = "RGAPI-82148cc6-c7a8-4d53-a38e-bb7959ca0c85"
+  const [malevolentTitanInfo, setMalevolentTitanInfo] = useState("");
+  const [malevolentTitanRanked, setMalevolentTitanRanked] = useState("");
+  function SearchMalevolentTitan(){
+    
+    var Summoner = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MalevolentTitan?api_key=' + apiKey
+
   
+    axios.get(Summoner).then(function (response){
+      setMalevolentTitanInfo(response.data);
+    }).catch(function (error) {
+      console.log(error)
+    })
+    console.log(malevolentTitanInfo.id)
+    var SummonerID = 'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/'+ malevolentTitanInfo.id +'?api_key=' + apiKey
+      axios.get(SummonerID).then(function (response1){
+        setMalevolentTitanRanked(response1.data);
+    }).catch(function (error) {
+      console.log(error)
+    })
+  }
+  
+
+
     return(
+
 
 <body class="body">
 
@@ -18,16 +46,18 @@ function App(){
     <img src={firePoroIcon} className="photo" alt="Avatar"/>
         <div class="container">
           <h2><b>MalevolentTitan</b></h2>
-          <button onclick="window.location='Summoner.html'">
+          <button onClick="">
             Search
           </button>
+          <h1>{malevolentTitanInfo.summonerLevel}</h1>
+          <h1>{malevolentTitanRanked.tier}</h1>
         </div>
     </div>
     <div class="card">
     <img src={helmetIcon} className="photo" alt="Avatar"/>
       <div class="container">
         <h2><b>Titans003</b></h2>
-        <button onclick="window.location='Summoner.html'">
+        <button onClick="">
           Search
         </button>
       </div>
@@ -36,7 +66,7 @@ function App(){
       <img src={LCSIcon} className="photo" alt="Avatar"/>
       <div class="container">
         <h2><b>Lexxx</b></h2>
-        <button onclick="window.location='Summoner.html'">
+        <button onClick="">
           Search
         </button>
       </div>
--- app/src/SearchBar.js
+++ app/src/SearchBar.js
@@ -5,20 +5,28 @@ import axios from 'axios';
 import "./SearchBar.css";
 
 
-const apiKey = "RGAPI-2fda1e97-7a97-48f6-92b9-2e0c2169b5df"
+const apiKey = "RGAPI-82148cc6-c7a8-4d53-a38e-bb7959ca0c85"
 function SearchBar(){
   const [summonerName, setSummonerName] = useState("");
   const [summonerInfo, setSummonerInfo] = useState("");
+  const [summonerRank, setSummonerRank] = useState("");
   
   function searchPlayer(event){
     var Summoner = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName + '?api_key=' + apiKey
     axios.get(Summoner).then(function (response){
+      console.log(response)
       setSummonerInfo(response.data);
       
     }).catch(function (error) {
       console.log(error)
     })
-  
+    var SummonerID = 'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/'+ summonerInfo.id +'?api_key=' + apiKey
+      axios.get(SummonerID).then(function (response1){
+        setSummonerRank(response1.data);
+    }).catch(function (error) {
+      console.log(error)
+    })
+    console.log(SummonerID)
   }
 
     return(
@@ -26,6 +34,7 @@ function SearchBar(){
         <input type="text" style={{ fontSize: "32px" }} placeholder="Search Summoner" onChange={e => setSummonerName(e.target.value)}/>
         <button type="submit" style={{ fontSize: "32px" }} onClick={e => searchPlayer(e)} >Submit</button>
         <h1>{summonerInfo.summonerLevel}</h1>
+        <h1>{summonerRank[0].tier}</h1>
   </div>
   
 
